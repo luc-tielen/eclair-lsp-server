b4bc41d6c8c2e3b795a7e451f97efb5ed47d2fe1
Luc Tielen <luc.tielen@gmail.com>
2023-08-27 14:47:17 +0200

    Setup building and running the LSP server

--- README.md
+++ README.md
@@ -5,10 +5,14 @@ Eclair.
 
 ## Dev setup
 
+- Install [Eclair](https://github.com/luc-tielen/eclair-lang) and make sure it
+  is available in your `$PATH`
 - Install nvm
 - Run the following commands:
 
   ```bash
   nvm use 18
   npm install
+  npm run build
+  npm start
   ```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
+import {
+  createConnection,
+  TextDocuments,
+  Diagnostic,
+  DiagnosticSeverity,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  CompletionItem,
+  CompletionItemKind,
+  TextDocumentPositionParams,
+  TextDocumentSyncKind,
+  InitializeResult,
+} from "vscode-languageserver/node.js";
+
 const main = () => {
   console.log("hello eclair lsp");
+  const connection = createConnection(ProposedFeatures.all);
+  // connection.onHover(TODO)
 };
 
 main();
